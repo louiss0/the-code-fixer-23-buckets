00429e72a3329d1f12e930974838c15b90b38fb3
louiss0 <louisshelton0@gmail.com>
2025-07-06 16:57:13 -0400

    fix: Update default upstream
    The default upstream was incorrect, and this commit updates it to the
    correct value.

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "louiss0/the-code-fixer-23-buckets:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
